cf97fdc521da6b7524966a0e68c15ac4e4c6af3e
Nipun Malinga Premalal <nipunmalinga5@gmail.com>
2025-02-08 01:21:59 +0530

    completed the authentication and tabs

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -11,11 +11,6 @@ export default function Index() {
       }}
     >
       <Text className="my-10 font-rubik-extraBold text-3xl">Welcome to the app</Text>
-      <Text className="my-10 font-rubik-light text-3xl">Welcome to the app</Text>
-      <Link href="/sign-in">Sign In</Link>
-      <Link href="/explore">Explore</Link>
-      <Link href="/profile">Profile</Link>
-      {/* <Link href="/properties/1">Property</Link> */}
     </View>
   );
 }
--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,81 +1,84 @@
-import {Avatars, Client, OAuthProvider , Account} from 'react-native-appwrite';
-import * as Linking from 'expo-linking';
+import { Avatars, Client, OAuthProvider, Account } from "react-native-appwrite";
+import * as Linking from "expo-linking";
 import { openAuthSessionAsync } from "expo-web-browser";
 
-export const config ={
-    platform: 'com.malinga.livva',
-    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
-    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
-}
+export const config = {
+  platform: "com.malinga.livva",
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+};
 
 export const client = new Client();
 
 client
-    .setEndpoint(config.endpoint!)
-    .setProject(config.projectId!)
-    .setPlatform(config.platform!)
+  .setEndpoint(config.endpoint!)
+  .setProject(config.projectId!)
+  .setPlatform(config.platform!);
 
 export const avatar = new Avatars(client);
 export const account = new Account(client);
 
-export async function login(){
-    try{
-        const redirectUri = Linking.createURL('/');
+export async function login() {
+  try {
+    const redirectUri = Linking.createURL("/");
 
-        const response = await account.createOAuth2Token(
-            OAuthProvider.Google,
-            redirectUri
-        );
-        if(!response) throw new Error('Failed to login');
+    const response = await account.createOAuth2Token(
+      OAuthProvider.Google,
+      redirectUri
+    );
+    if (!response) throw new Error("Failed to login");
 
-        const browserResult = await openAuthSessionAsync(
-            response.toString(),
-            redirectUri
-        )
+    const browserResult = await openAuthSessionAsync(
+      response.toString(),
+      redirectUri
+    );
 
-        if(browserResult.type !== 'success') throw new Error('Failed to login');
+    if (browserResult.type !== "success") throw new Error("Failed to login1");
 
-        const url = new URL (browserResult.url);
+    const url = new URL(browserResult.url);
 
-        const secret = url.searchParams.get('secret')?.toString();
-        const userId = url.searchParams.get('userId')?.toString();
+    const secret = url.searchParams.get("secret")?.toString();
+    const userId = url.searchParams.get("userId")?.toString();
 
-        if(!secret || !userId) throw new Error('Failed to login');
+    console.log("secretId and used:" + secret, userId);
 
-        const session = await account.createSession(userId, secret);
+    if (!secret || !userId) throw new Error("Failed to login2");
 
-        if(!session) throw new Error('Failed to create a session');
-    }catch(error){
-        console.log(error);
-        return false;
-    }
+    const session = await account.createSession(userId, secret);
+
+    if (!session) throw new Error("Failed to create a session");
+    
+    return true;
+
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
 }
 
-export async function logout(){
-    try{
-        await account.deleteSession('current');
-        return true;
-    }
-    catch(error){
-        console.log(error);
-        return false;
-    }
+export async function logout() {
+  try {
+    await account.deleteSession("current");
+    return true;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
 }
 
-export async function getCurrentUser(){
-    try{
-        const response = await account.get();
-        if(response.$id){
-            const userAvatar = avatar.getInitials(response.name);
-            return{
-                ...response,
-                avatar: userAvatar.toString(),
-            }
-        }
-        return response;
+export async function getCurrentUser() {
+  try {
+    const response = await account.get();
+    if (response.$id) {
+      const userAvatar = avatar.getInitials(response.name);
+      return {
+        ...response,
+        avatar: userAvatar.toString(),
+      };
     }
-    catch(error){
-        console.log(error);
-        return false;
-    }
+    return response;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
 }
