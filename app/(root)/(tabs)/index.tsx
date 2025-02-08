f4205f5e31871de8800bfb1a5c7d4c19e80e0739
Nipun Malinga Premalal <nipunmalinga5@gmail.com>
2025-02-08 18:00:28 +0530

    profile page completed

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,16 +1,28 @@
-import { Text, View } from "react-native";
+import { SafeAreaView, Text, View, Image } from "react-native";
 import { Link } from "expo-router";
+import images from "@/constants/images";
+import icons from "@/constants/icons";
+import Search from "@/components/Search";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="my-10 font-rubik-extraBold text-3xl">Welcome to the app</Text>
-    </View>
+    <SafeAreaView className="h-full bg-white">
+      <View className="px-5">
+        <View className="flex flex-row items-ceter justify-between mt-5">
+          <View className="flex flex-row items-center">
+            <Image source={images.avatar} className="size-12" rounded-full></Image>
+            <View className="flex flex-col items-start ml-2 justify-center">
+              <Text className="text-xs font-rubik text-black-100">Good Morning</Text>
+              <Text className="text-base font-rubik-medium text-black-300">Malinga</Text>
+            </View>
+          </View>
+
+          <Image source={icons.bell} className="size-6"></Image>
+
+        </View>
+      </View>
+
+      <Search />
+    </SafeAreaView>
   );
 }
